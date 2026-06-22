5382ab7362145e8d47a7bd50d0ed01024d5fb3f4
Ziegan <zieganwes@gmail.com>
2026-06-22 13:21:44 +0530

    Cleanup 2026-06-26

--- AI-ML/LANGCHAIN/01-Intro.py
+++ AI-ML/LANGCHAIN/01-Intro.py
@@ -1,4 +1,27 @@
+import requests
 from langchain_ollama import ChatOllama
 
-llm = ChatOllama(model="qwen-stable:latest")
-print(llm.invoke(input("Enter the Query For the LLM:")).content)
+
+def llm_calling(input_data, model_to_use):
+    llm = ChatOllama(model=model_to_use)
+    return print(llm.invoke(input_data).content)
+
+
+base_url = "http://localhost:11434/"
+response_data = requests.get(base_url + "/api/tags")
+available_models_list = {}
+i = 0
+for models in response_data.json()["models"]:
+    available_models_list[i] = models["name"]
+    i += 1
+print(available_models_list)
+
+print("Select the model to chat:\n")
+for key, value in available_models_list.items():
+    print(key, value)
+choice = input("Enter the model number:")
+if int(choice) in available_models_list:
+    selected_model = available_models_list[int(choice)]
+    llm_calling(input("Enter the Query:"), selected_model)
+else:
+    print("Invalid Model Selection")
